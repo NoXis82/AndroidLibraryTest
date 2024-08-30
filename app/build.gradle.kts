d3dc46a4bf9687a1fb2766661438f40785928966
Tumanov Vycheslav <63115274+NoXis82@users.noreply.github.com>
2024-08-30 16:48:04 +0300

    refactoring

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.noxis.androidlibrarytest"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
